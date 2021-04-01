3f4091f4cae452090f96abba230918d1f4abac78
paul77uk <paul77uk@gmail.com>
2021-04-01 10:06:27 +0100

    fourth commit

--- app/src/main/java/com/udacity/asteroidradar/api/PicOfDayApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/PicOfDayApiService.kt
@@ -7,6 +7,7 @@ import com.udacity.asteroidradar.PictureOfDay
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -14,13 +15,13 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .baseUrl("$BASE_URL/")
+        .baseUrl(BASE_URL)
         .build()
 
 interface PicApiService {
 
-    @GET("url")
-    suspend fun getPhoto(): PictureOfDay
+    @GET("planetary/apod")
+    suspend fun getPhoto(@Query("api_key") apiKey: String = "G72leUEWda5TavF1K49jqAbwPEoj0oVsp0DxcYSu"): PictureOfDay
 }
 
 object PicApi {
