8782e0a374c5d1ec8057ad8cbc6039415d466238
paul77uk <paul77uk@gmail.com>
2021-04-14 11:39:05 +0100

    eigth commit

--- app/src/main/java/com/udacity/asteroidradar/api/ApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/ApiService.kt
@@ -7,17 +7,23 @@ import com.udacity.asteroidradar.Constants.BASE_URL
 import com.udacity.asteroidradar.PictureOfDay
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .baseUrl(BASE_URL)
-        .build()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+private val retrofit2 = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
 
 interface PicApiService {
 
@@ -29,7 +35,6 @@ object PicApi {
     val retrofitService: PicApiService by lazy {
         retrofit.create(PicApiService::class.java)
     }
-    
 }
 
 interface FeedApiService {
@@ -39,7 +44,6 @@ interface FeedApiService {
 
 object FeedApi {
     val retrofitService: FeedApiService by lazy {
-        retrofit.create(FeedApiService::class.java)
+        retrofit2.create(FeedApiService::class.java)
     }
-
 }
