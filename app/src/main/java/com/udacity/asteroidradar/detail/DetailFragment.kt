4f79e75a0c1168e691fb349d0df56905c4d75efc
paul77uk <paul77uk@gmail.com>
2021-04-27 13:35:49 +0100

    fifteenth commit

--- app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentDetailBinding
 
@@ -16,16 +17,17 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val application = requireNotNull(activity).application
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
         val asteroid = DetailFragmentArgs.fromBundle(requireArguments()).selectedAsteroid
+        val application = requireNotNull(activity).application
         val viewModelFactory = DetailViewModelFactory(asteroid, application)
-//        binding.viewModel = ViewModelProvider(
-//            this, viewModelFactory).get(DetailViewModel::class.java)
 
-        binding.asteroid = asteroid
+        binding.viewModel = ViewModelProvider(
+            this, viewModelFactory).get(DetailViewModel::class.java)
+
+//        binding.asteroid = asteroid
 
         binding.helpButton.setOnClickListener {
             displayAstronomicalUnitExplanationDialog()
--- app/src/main/java/com/udacity/asteroidradar/detail/DetailViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailViewModel.kt
@@ -4,9 +4,17 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.database.getDatabase
 import com.udacity.asteroidradar.domain.Asteroid
+import com.udacity.asteroidradar.repository.AsteroidsRepository
+import kotlinx.coroutines.launch
 
 class DetailViewModel(asteroid: Asteroid, app: Application) : AndroidViewModel(app)  {
+
+    private val database = getDatabase(app)
+
+    private val asteroidsRepository = AsteroidsRepository(database)
 
     private val _selectedAsteroid = MutableLiveData<Asteroid>()
     val selectedAsteroid: LiveData<Asteroid>
@@ -14,5 +22,8 @@ class DetailViewModel(asteroid: Asteroid, app: Application) : AndroidViewModel(a
 
     init {
         _selectedAsteroid.value = asteroid
+        viewModelScope.launch {
+            asteroidsRepository.refreshAsteroids()
+        }
     }
 }
